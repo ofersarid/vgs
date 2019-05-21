4e6748260202835676fd2a5fd18f621229cb226e
ofersarid <ofersarid@gmail.com>
2019-05-21 19:08:12 +0300

    component layout - with dummy content

--- src/components/two-column-layout/two-column-layout.js
+++ src/components/two-column-layout/two-column-layout.js
@@ -6,12 +6,11 @@ import Device from '/src/components/device';
 import { SnapScroll, ScrollableArea } from '/src/components/index';
 import styles from './styles.scss';
 
-const TwoColumnLayout = ({ index, frame, header, firstLook, article, footNotes }) => {
+const TwoColumnLayout = ({ index, frame, header, firstLook, article, footNotes, isMobile }) => {
   const forward = frame >= index;
 
   const resolveSpring = () => {
     return frame !== index ? useSpring({
-      y: forward ? -100 : 100,
       x: forward ? 300 : -300,
       o: 0,
       from: {
@@ -24,7 +23,6 @@ const TwoColumnLayout = ({ index, frame, header, firstLook, article, footNotes }
       x: 0,
       o: 1,
       from: {
-        y: forward ? 100 : -100,
         x: forward ? -100 : 100,
         o: 0,
       },
@@ -39,22 +37,33 @@ const TwoColumnLayout = ({ index, frame, header, firstLook, article, footNotes }
         <h2 className={styles.header} >
           <animated.span className={styles.index} style={{
             opacity: o,
-            transform: x.interpolate(x => `translateX(${x}%)`),
+            transform: isMobile ? x.interpolate(x => `translateX(${x}%)`) : x.interpolate(x => `translateY(${-x}%)`),
           }} >0{index}</animated.span >
           <animated.span className={styles.label} style={{
             opacity: o,
-            // transform: y.interpolate(y => `translateY(${y}%)`),
-          }} >{header}</animated.span >
+          }} >
+            <animated.span className={styles.divider} style={{
+              opacity: o,
+            }} />
+            {header}
+          </animated.span >
         </h2 >
         <animated.section className={styles.content} style={{ opacity: o }} >
           <ScrollableArea index={index}>
             <div className={styles.article}>
               {article}
             </div>
+            {isMobile && (
+              <ol className={styles.footNotes}>
+                {footNotes.map((note, i) => <li key={i}>{note}</li>)}
+              </ol>
+            )}
+          </ScrollableArea>
+          {!isMobile && (
             <ol className={styles.footNotes}>
               {footNotes.map((note, i) => <li key={i}>{note}</li>)}
             </ol>
-          </ScrollableArea>
+          )}
         </animated.section>
       </div >
     </div >
@@ -68,6 +77,7 @@ TwoColumnLayout.propTypes = {
   firstLook: PropTypes.bool.isRequired,
   article: PropTypes.string,
   footNotes: PropTypes.arrayOf(PropTypes.string),
+  isMobile: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -75,6 +85,7 @@ const mapStateToProps = state => ({
   deviceOrientation: Device.selectors.deviceOrientation(state),
   frame: SnapScroll.selectors.frame(state),
   firstLook: SnapScroll.selectors.firstLook(state),
+  isMobile: Device.selectors.isMobile(state),
 });
 
 const mapDispatchToProps = dispatch => ({}); // eslint-disable-line
