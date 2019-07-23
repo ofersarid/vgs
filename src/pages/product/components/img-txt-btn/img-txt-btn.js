58b0925dd038f7f33ae0d51c1f94fad704a7da92
ofersarid <ofersarid@gmail.com>
2019-07-23 21:23:58 +0300

    publish

--- src/pages/product/components/img-txt-btn/img-txt-btn.js
+++ src/pages/product/components/img-txt-btn/img-txt-btn.js
@@ -8,6 +8,8 @@ import styles from './styles.scss';
 import { Spring } from 'react-spring/renderprops-universal';
 import { SnapScroll, ScrollableArea, Button } from '/src/shared';
 import Device from '/src/shared/device';
+// import activityIndicator from '/src/svg-loaders/ball-triangle.svg';
+// import ReactSVG from 'react-svg';
 import sharedStyles from '../../styles.scss';
 import Footnotes from '../footnotes/footnotes';
 
@@ -15,10 +17,18 @@ class ImgTxtBtn extends PureComponent {
   constructor(props) {
     super(props);
     autoBind(this);
+    this.state = {
+      isLoaded: false,
+    };
+  }
+
+  hideSpinner() {
+    this.setState({ isLoaded: true });
   }
 
   render() {
-    const { imgSubTitle, showOnFrame, frame, img, txt, pdfSrc, themeColor, footNotes, isTouchDevice } = this.props;
+    const { imgSubTitle, showOnFrame, frame, img, youtube, txt, pdfSrc, themeColor, footNotes, isTouchDevice } = this.props;
+    const { isLoaded } = this.state;
     return (
       <Spring
         from={{ opacity: 0 }}
@@ -33,27 +43,38 @@ class ImgTxtBtn extends PureComponent {
           }}
         >
           <div className={cx(styles.img)} >
-            {isTouchDevice ? <img src={img} className={styles.inner} />
-              : <div style={{ backgroundImage: `url(${img})` }} className={styles.inner} />
-            }
-            <div className={cx(styles.title)} >{imgSubTitle}</div >
+            {img && isTouchDevice && <img src={img} className={styles.inner} onLoad={this.hideSpinner} />}
+            {img && !isTouchDevice && <div style={{ backgroundImage: `url(${img})` }} className={styles.inner} />}
+            {imgSubTitle && <div className={cx(styles.title)} >{imgSubTitle}</div >}
+            {youtube && (
+              <iframe
+                src={`https://www.youtube.com/embed/${youtube.split('v=')[1]}?loop=1&modestbranding=1&showinfo=0&theme=light&disablekb=1`}
+                width="560" height="315" frameBorder="0"
+                onLoad={this.hideSpinner}
+                style={{
+                  opacity: isLoaded ? 1 : 0,
+                }}
+              />
+            )}
           </div >
           <div className={cx(styles.rightCol)} >
             <p className={cx(styles.txt)} dangerouslySetInnerHTML={{ __html: txt.replace(/\n\r?/g, '<br />') }} />
-            <Button
-              tag="a"
-              color
-              target="_blank"
-              rel="noopener noreferrer"
-              href={pdfSrc}
-              waveColor="white"
-              className={cx(styles.btn)}
-              style={{
-                background: themeColor,
-              }}
-            >
-              PRODUCT PDF
-            </Button >
+            {pdfSrc && (
+              <Button
+                tag="a"
+                color
+                target="_blank"
+                rel="noopener noreferrer"
+                href={pdfSrc}
+                waveColor="white"
+                className={cx(styles.btn)}
+                style={{
+                  background: themeColor,
+                }}
+              >
+                PRODUCT PDF
+              </Button >
+            )}
           </div >
           <Footnotes footNotes={footNotes} />
         </ScrollableArea >}
@@ -63,11 +84,12 @@ class ImgTxtBtn extends PureComponent {
 }
 
 ImgTxtBtn.propTypes = {
-  img: PropTypes.string.isRequired,
-  imgSubTitle: PropTypes.string.isRequired,
+  img: PropTypes.string,
+  youtube: PropTypes.string,
+  imgSubTitle: PropTypes.string,
   txt: PropTypes.string.isRequired,
   footNotes: PropTypes.arrayOf(PropTypes.string).isRequired,
-  pdfSrc: PropTypes.string.isRequired,
+  pdfSrc: PropTypes.string,
   themeColor: PropTypes.string.isRequired,
   frame: PropTypes.number.isRequired,
   showOnFrame: PropTypes.number.isRequired,
--- src/pages/product/product.js
+++ src/pages/product/product.js
@@ -28,10 +28,12 @@ class Product extends PureComponent {
       <Fragment >
         <IndexHeader index={1} header={data.screen1Title} color={color} />
         <IndexHeader index={2} header={data.screen2Title} color={color} />
-        <IndexHeader index={3} header="Key Features" color={color} />
-        <IndexHeader index={4} header="Two Pictures Layout" color={color} />
-        <IndexHeader index={5} header="Summary" color={color} />
-        <IndexHeader index={6} header="Clinical" />
+        <IndexHeader index={3} header={data.screen3Title} color={color} />
+        <IndexHeader index={4} header="Key Features" color={color} />
+        <IndexHeader index={5} header="Two Pictures Layout" color={color} />
+        <IndexHeader index={6} header="Summary" color={color} />
+        <IndexHeader index={7} header="Downloads" color={color} />
+        <IndexHeader index={8} header="Clinical" />
         <SnapScroll >
           <Cover
             art={art}
@@ -52,7 +54,23 @@ class Product extends PureComponent {
             footNotes={[data.screen1Footnote1, data.screen1Footnote2, data.screen1Footnote3]}
             showOnFrame={1}
           />
-          <ThreeColumnLayout showOnFrame={2} data={[{
+          <ImgTxtBtn
+            img={data.screen2Image}
+            txt={data.screen2Body}
+            footNotes={[data.screen2Footnote1, data.screen2Footnote2, data.screen2Footnote3]}
+            pdfSrc={data.screen2PDF}
+            imgSubTitle={data.screen2ImageSubtitle}
+            showOnFrame={2}
+            themeColor={color}
+          />
+          <ImgTxtBtn
+            youtube={data.screen3Videolink}
+            txt={data.screen3Body}
+            footNotes={[data.screen3Footnote1, data.screen3Footnote2, data.screen3Footnote3]}
+            showOnFrame={3}
+            themeColor={color}
+          />
+          <ThreeColumnLayout showOnFrame={4} data={[{
             id: '1',
             text: 'A variety of models'
           }, {
@@ -71,25 +89,16 @@ class Product extends PureComponent {
             id: '6',
             text: 'A variety of models compatible with veins 3.5-8.0 mm in diameter, variety of models'
           }]} />
-          <ImgTxtBtn
-            img={data.screen2Image}
-            txt={data.screen2Body}
-            footNotes={[data.screen2Footnote1, data.screen2Footnote2, data.screen2Footnote3]}
-            pdfSrc={data.screen2PDF}
-            imgSubTitle={data.screen2ImageSubtitle}
-            showOnFrame={3}
-            themeColor={color}
-          />
           <TwoImagesLayout
-            showOnFrame={4}
+            showOnFrame={5}
             image1="https://www.deadlineclaims.com/wp-content/uploads/2017/02/placeholder-image.jpg"
             image2="https://www.deadlineclaims.com/wp-content/uploads/2017/02/placeholder-image.jpg"
             img1Description="A variety of models compatible with veins 3.5-8.0 mm in diameter, variety of models"
             img2Description="A variety of models compatible with veins 3.5-8.0 mm in diameter, variety of models"
           />
-          <Summary showOnFrame={5} />
+          <Summary showOnFrame={6} />
           <Downloads
-            showOnFrame={6}
+            showOnFrame={7}
             image="https://www.deadlineclaims.com/wp-content/uploads/2017/02/placeholder-image.jpg"
             imageTitle="Example Title"
             brochure="http://www.africau.edu/images/default/sample.pdf"
@@ -99,7 +108,7 @@ class Product extends PureComponent {
             themeColor={color}
 
           />
-          <Clinical showOnFrame={7} themeColor={color} />
+          <Clinical showOnFrame={8} themeColor={color} />
         </SnapScroll >
       </Fragment >
     ) : null;
@@ -111,22 +120,7 @@ Product.propTypes = {
   color: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   art: PropTypes.string.isRequired,
-  data: PropTypes.shape({
-    coverTagLine: PropTypes.string.isRequired,
-    screen1Title: PropTypes.string.isRequired,
-    screen1Body: PropTypes.string.isRequired,
-    screen1Footnote1: PropTypes.string,
-    screen1Footnote2: PropTypes.string,
-    screen1Footnote3: PropTypes.string,
-    screen2Title: PropTypes.string.isRequired,
-    screen2Image: PropTypes.string.isRequired,
-    screen2ImageSubtitle: PropTypes.string,
-    screen2Body: PropTypes.string.isRequired,
-    screen2PDF: PropTypes.string.isRequired,
-    screen2Footnote1: PropTypes.string,
-    screen2Footnote2: PropTypes.string,
-    screen2Footnote3: PropTypes.string,
-  }),
+  data: PropTypes.object,
 };
 
 const mapStateToProps = state => ({
@@ -142,3 +136,271 @@ const mapStateToProps = state => ({
 export default compose(
   connect(mapStateToProps, {}),
 )(Product);
+
+// const schema = [{
+//   key: 'coverTagLine',
+//   label: 'Cover tagline',
+//   maxChars: 70,
+//   required: true,
+//   type: 'multi-line-preserve-lines',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'eventTitle',
+//   label: 'Main event title',
+//   maxChars: 60,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'eventDateFrom',
+//   label: 'Date - Start',
+//   required: true,
+//   type: 'date',
+//   validateWith: 'date-time',
+// }, {
+//   key: 'eventDateTo',
+//   label: 'Date - End',
+//   required: true,
+//   type: 'date',
+//   validateWith: 'date-time',
+// }, {
+//   key: 'eventAddress',
+//   label: 'Location',
+//   maxChars: 30,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'eventLinkTo',
+//   label: 'URL link',
+//   required: true,
+//   type: 'link',
+//   validateWith: 'link',
+// }, {
+//   key: 'screen1Title',
+//   label: 'Screen title',
+//   maxChars: 50,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen1Body',
+//   label: 'Body',
+//   maxChars: 950,
+//   required: true,
+//   type: 'multi-line-preserve-lines',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen1Footnote1',
+//   label: 'Footnote 1',
+//   maxChars: 250,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen1Footnote2',
+//   label: 'Footnote 2',
+//   maxChars: 250,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen1Footnote3',
+//   label: 'Footnote 3',
+//   maxChars: 250,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen2Title',
+//   label: 'Screen title',
+//   maxChars: 50,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, { key: 'screen2Image', label: 'Image', required: true, type: 'image', }, {
+//   key: 'screen2ImageSubtitle',
+//   label: 'Image subtitle',
+//   maxChars: 50,
+//   required: false,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen2Body',
+//   label: 'Body',
+//   maxChars: 450,
+//   required: true,
+//   type: 'multi-line-preserve-lines',
+//   validateWith: 'min-max',
+// }, { key: 'screen2PDF', label: 'PDF', required: true, type: 'pdf', }, {
+//   key: 'screen2Footnote1',
+//   label: 'Footnote 1',
+//   maxChars: 250,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen2Footnote2',
+//   label: 'Footnote 2',
+//   maxChars: 250,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen2Footnote3',
+//   label: 'Footnote 3',
+//   maxChars: 250,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// },
+// {
+//   key: 'screen3Title',
+//   label: 'Screen title',
+//   maxChars: 50,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen3Videolink',
+//   label: 'YOUTUBE link',
+//   required: true,
+//   type: 'link',
+//   validateWith: 'link',
+// }, {
+//   key: 'screen3Body',
+//   label: 'Body',
+//   maxChars: 950,
+//   required: true,
+//   type: 'multi-line-preserve-lines',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen4Title',
+//   label: 'Screen title',
+//   maxChars: 50,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen4Bullet1',
+//   label: 'Bullet 1',
+//   maxChars: 170,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen4Bullet2',
+//   label: 'Bullet 2',
+//   maxChars: 170,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen4Bullet3',
+//   label: 'Bullet 3',
+//   maxChars: 170,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen4Bullet4',
+//   label: 'Bullet 4',
+//   maxChars: 170,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen4Bullet5',
+//   label: 'Bullet 5',
+//   maxChars: 170,
+//   required: false,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen4Bullet6',
+//   label: 'Bullet 6',
+//   maxChars: 170,
+//   required: false,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen5Title',
+//   label: 'Screen title',
+//   maxChars: 50,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, { key: 'screen5Image1', label: 'Image 1', required: true, type: 'image', }, {
+//   key: 'screen5Image1Subtitle',
+//   label: 'Image subtitle',
+//   maxChars: 50,
+//   required: false,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, { key: 'screen5Image2', label: 'Image 2', required: true, type: 'image', }, {
+//   key: 'screen5Image2Subtitle',
+//   label: 'Image subtitle',
+//   maxChars: 50,
+//   required: false,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen6Title',
+//   label: 'Screen title',
+//   maxChars: 50,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, { key: 'screen6Image', label: 'Image 2', required: true, type: 'image', }, {
+//   key: 'screen6ImageSubtitle',
+//   label: 'Image subtitle',
+//   maxChars: 50,
+//   required: false,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, { key: 'brochure', label: 'Brochure PDF', required: false, type: 'pdf', }, {
+//   key: 'ifu',
+//   label: 'IFU PDF',
+//   required: false,
+//   type: 'pdf',
+// }, { key: 'patientCard', label: 'Patiend card PDF', required: false, type: 'pdf', }, {
+//   key: 'instructions',
+//   label: 'Reprocessing instructions PDF',
+//   required: false,
+//   type: 'pdf',
+// }, {
+//   key: 'screen7Title',
+//   label: 'Screen title',
+//   maxChars: 50,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen7Bullet1',
+//   label: 'Bullet 1',
+//   maxChars: 170,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen7Bullet2',
+//   label: 'Bullet 2',
+//   maxChars: 170,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen7Bullet3',
+//   label: 'Bullet 3',
+//   maxChars: 170,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }, {
+//   key: 'screen7Bullet4',
+//   label: 'Bullet 4',
+//   maxChars: 170,
+//   required: true,
+//   type: 'multi-line',
+//   validateWith: 'min-max',
+// }];
