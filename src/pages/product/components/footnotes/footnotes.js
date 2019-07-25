27f417cbaf941afa967d7bb00c31ff7fefc7a50b
ofersarid <ofersarid@gmail.com>
2019-07-25 12:55:35 +0300

    publish

--- src/pages/product/components/footnotes/footnotes.js
+++ src/pages/product/components/footnotes/footnotes.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import cx from 'classnames';
+import cx from 'classnames';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 // import PropTypes from 'prop-types';
 import styles from './styles.scss';
 
-const Footnotes = ({ footNotes }) => (
-  <ol className={styles.footNotes} >
-    {footNotes.map((note, i) => note ? <li key={i} >{note}</li > : null)}
-  </ol >
-);
+const Footnotes = ({ footNotes }) => {
+  let isEmpty = true;
+  footNotes.forEach(item => {
+    if (item) {
+      isEmpty = false;
+    }
+  });
+  return (
+    <ol className={cx(styles.footNotes, { [styles.hideTopLine]: isEmpty })} >
+      {footNotes.map((note, i) => note ? <li key={i} >{note}</li > : null)}
+    </ol >
+  );
+};
 
 Footnotes.propTypes = {
   footNotes: PropTypes.arrayOf(PropTypes.string),
