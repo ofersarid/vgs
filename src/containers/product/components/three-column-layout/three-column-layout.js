ed6dc791c57a5713bee063ad98ea0b0863629917
ofersarid <ofersarid@gmail.com>
2019-09-10 11:08:21 +0300

    publish

--- src/containers/product/components/three-column-layout/three-column-layout.js
+++ src/containers/product/components/three-column-layout/three-column-layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 import PropTypes from 'prop-types';
-import { ScrollableArea, FadeIn } from '/src/shared';
+import { FadeIn } from '/src/shared';
 import styles from './styles.scss';
 import sharedStyles from '../../styles.scss';
 
@@ -9,11 +9,11 @@ const ThreeColumnLayout = ({ data }) => {
   return (
     <FadeIn spread >
       <section className={cx(styles.content, sharedStyles.inner)} >
-        <ScrollableArea className={styles.list} >
+        <div className={styles.list} >
           {data.map(item => item ? (
             <div key={item} className={styles.listItem} >{item}</div >
           ) : null)}
-        </ScrollableArea >
+        </div >
       </section >
     </FadeIn>
   );
