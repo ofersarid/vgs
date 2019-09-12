bfc4bdb5a1fa512bab83abdee6f1748f91bf90e5
ofersarid <ofersarid@gmail.com>
2019-09-12 23:02:14 +0300

    publish

--- src/containers/main/main.js
+++ src/containers/main/main.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import autoBind from 'auto-bind';
 import PropTypes from 'prop-types';
-import { Spring } from 'react-spring/renderprops';
+import { Spring, config } from 'react-spring/renderprops';
 import ReduxRoutes from '/src/routes/components/redux-routes/redux-routes';
 import Routes from '/src/routes';
 import Device from '/src/shared/device';
@@ -18,7 +18,7 @@ import SideMenu from './components/side-menu';
 import Card from './components/card';
 import { logoGreen, vgsGreen } from './assets';
 import cx from 'classnames';
-import { Button } from '/src/shared';
+import { Button, SnapScroll } from '/src/shared';
 
 class Main extends PureComponent {
   constructor(props) {
@@ -63,7 +63,7 @@ class Main extends PureComponent {
 
   render() {
     const show = this.props.pathname !== '/product';
-    const { pathname, logo, bizCard, orientation } = this.props;
+    const { pathname, logo, bizCard, orientation, isLastFrame, color, frame } = this.props;
     const isMobile = utils.isMobile();
     return (
       <Fragment >
@@ -101,6 +101,20 @@ class Main extends PureComponent {
                 )}
               {(orientation === 'landscape' && isMobile) ? null : <SideMenu />}
               {(orientation === 'landscape' && isMobile) ? null : <FrameIndicator />}
+              <Spring
+                from={{ opacity: isLastFrame ? 0 : 1, transform: `translateY(${isLastFrame ? '100%' : '0%'})` }}
+                to={{ opacity: isLastFrame ? 1 : 0, transform: `translateY(${isLastFrame ? '0%' : '100%'})` }}
+                config={config.slow}
+                immediate={frame === 0}
+              >
+                {springs => <div style={{
+                  ...springs,
+                  background: color,
+                }} className={styles.footer}>
+                  <span>&copy;2019 all rights reserved to VGS</span>
+                  <span>Produced by</span>
+                </div>}
+              </Spring>
               {this.props.children}
             </div >}
           </Spring >
@@ -116,6 +130,7 @@ Main.propTypes = {
   pathname: PropTypes.string,
   color: PropTypes.string,
   isMobile: PropTypes.bool.isRequired,
+  isLastFrame: PropTypes.bool.isRequired,
   logo: PropTypes.string,
   resourceList: PropTypes.shape({
     collections: PropTypes.arrayOf(PropTypes.string).isRequired,
@@ -126,6 +141,7 @@ Main.propTypes = {
   orientation: PropTypes.oneOf(['portrait', 'landscape']),
   setColor: PropTypes.func.isRequired,
   updateLastFrame: PropTypes.func.isRequired,
+  frame: PropTypes.number.isRequired,
 };
 
 Main.defaultProps = {
@@ -140,6 +156,8 @@ const mapStateToProps = state => ({
   bizCard: services.vgs.selectors.bizCard(state),
   orientation: services.vgs.selectors.orientation(state),
   color: services.vgs.selectors.color(state),
+  isLastFrame: SnapScroll.selectors.isLastFrame(state),
+  frame: SnapScroll.selectors.frame(state),
 });
 
 const mapDispatchToProps = dispatch => ({
