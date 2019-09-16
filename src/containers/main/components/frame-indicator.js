eb31ef86ddd04157f3ac49afcbcd7228c1918979
ofersarid <ofersarid@gmail.com>
2019-09-16 16:28:47 +0300

    publish

--- src/containers/main/components/frame-indicator.js
+++ src/containers/main/components/frame-indicator.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import autoBind from 'auto-bind';
+import Device from '/src/shared/device';
 import { connect } from 'react-redux';
 import { SnapScroll } from '/src/shared';
 import 'babel-polyfill';
@@ -23,14 +24,15 @@ class FrameIndicator extends PureComponent {
   }
 
   render() {
-    const { frame, color, count } = this.props;
+    const { frame, color, count, isMobile } = this.props;
 
     return count > 1 ? (
       <ul className={styles.frameIndicator} >
         {this.renderBullets()}
         <div className={styles.indicate} style={{
-          top: 18 * frame,
+          top: isMobile ? `${100 * frame / count}%` : (18 * frame),
           backgroundColor: color,
+          height: isMobile ? `${100 / count}%` : undefined,
         }} />
       </ul >
     ) : null;
@@ -41,12 +43,14 @@ FrameIndicator.propTypes = {
   count: PropTypes.number.isRequired,
   frame: PropTypes.number.isRequired,
   color: PropTypes.string.isRequired,
+  isMobile: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = state => ({
   count: SnapScroll.selectors.count(state),
   frame: SnapScroll.selectors.frame(state),
   color: services.vgs.selectors.color(state),
+  isMobile: Device.selectors.isMobile(state),
 });
 
 const mapDispatchToProps = dispatch => ({});
