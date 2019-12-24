bb927fc8c5c89b44f51b0fc28b80f1bbd29b1eed
ofersarid <ofersarid@gmail.com>
2019-12-24 15:00:22 +0200

    publish

--- src/shared/fade-in/fade-in.js
+++ src/shared/fade-in/fade-in.js
@@ -1,48 +1,24 @@
 import React from 'react';
 import cx from 'classnames';
-import { Spring, config as conf } from 'react-spring/renderprops-universal';
 import PropTypes from 'prop-types';
 import styles from './styles.scss';
 
-const FadeIn = ({ children, slideFrom, spread, className, config }) => {
-  const resolveOrientation = () => {
-    switch (slideFrom) {
-      case 'right':
-      case 'left':
-      default:
-        return 'X';
-    }
-  };
+const FadeIn = ({ children, spread, className }) => {
   return (
-    <Spring
-      from={{ opacity: 0, x: slideFrom ? 10 : 0 }}
-      to={{ opacity: 1, x: 0 }}
-      config={conf[config]}
-    >
-      {spring => <div
-        className={cx(className, { [styles.spread]: spread })}
-        style={{
-          opacity: spring.opacity,
-          transform: `translate${resolveOrientation()}(${slideFrom === 'left' ? '-' : ''}${spring.x}%)`,
-        }}
-      >
-        {children}
-      </div >}
-    </Spring >
+    <div className={cx(styles.fadeIn, className, { [styles.spread]: spread })} >
+      {children}
+    </div >
   );
 };
 
 FadeIn.propTypes = {
   children: PropTypes.any,
-  slideFrom: PropTypes.oneOf(['right', 'left']),
   spread: PropTypes.bool,
   className: PropTypes.string,
-  config: PropTypes.oneOf(['default', 'slow', 'gentle', 'wobbly', 'stiff', 'slow', 'molasses']),
 };
 
 FadeIn.defaultProps = {
   spread: false,
-  config: 'default',
 };
 
 export default FadeIn;
