93b727f1b648c5e33efddc2c8fb47b4efe71111f
ofersarid <ofersarid@gmail.com>
2021-08-30 11:17:13 +0300

    small refactor for distributors and reactor v2

--- src/containers/contact/distributors.js
+++ src/containers/contact/distributors.js
@@ -28,79 +28,94 @@ const Distributors = ({ data, color }) => {
     }
   };
 
-  const dataSorted = sortBy(data, d => d.country);
+  const dataSorted = sortBy(data, (d) => d.country);
 
   return (
-    <FadeIn className={cx({ [layout.inner]: utils.isMobile(), [styles.inner]: utils.isMobile() })} >
+    <FadeIn
+      className={cx({
+        [layout.inner]: utils.isMobile(),
+        [styles.inner]: utils.isMobile()
+      })}
+    >
       <Carousel
         displayVolume={resolveVolume()}
         className={styles.carousel}
         color={color}
-        navLocation="bottom"
+        navLocation='bottom'
         prevBtnTxt={utils.isMobile() ? undefined : 'BACK'}
         nextBtnTxt={utils.isMobile() ? undefined : 'MORE'}
       >
-        {dataSorted.map(item => (
-          <div className={styles.item} key={item.distributor} >
-            <div className={styles.itemInner} >
-              <p className={styles.country} >{item.country}</p >
-              <p className={styles.products} >{JSON5.parse(item.products).reduce((reuduced, itm) => {
-                if (itm.active) {
-                  reuduced.push(itm.view);
-                }
-                return reuduced;
-              }, []).join(' | ')}</p >
-              <p >{item.distributor}</p >
-              <p >{item.phone}</p >
-              <section className={styles.btns} >
-                {item.email && (<Button
-                  tag="a"
-                  withBorder
-                  target="_blank"
-                  href={`mailto:${item.email}`}
-                >
-                  <AtSign />
-                </Button >)}
-                {item.link && (<Button
-                  tag="a"
-                  withBorder
-                  target="_blank"
-                  href={item.link}
-                >
-                  <Globe />
-                </Button >)}
-                {item.googleLocation && (<Button
-                  tag="a"
-                  withBorder
-                  target="_blank"
-                  href={item.googleLocation}
-                >
-                  <MapPin />
-                </Button >)}
-              </section >
-            </div >
-          </div >
+        {dataSorted.map((item) => (
+          <div className={styles.item} key={item.distributor}>
+            <div className={styles.itemInner}>
+              <p className={styles.country}>{item.country}</p>
+              <p className={styles.products}>
+                {[
+                  { txt: 'VEST', included: item.vest },
+                  { txt: 'VIOLA', included: item.viola },
+                  { txt: 'FRAME', included: item.frame },
+                  { txt: 'FRAME-FR', included: item.frame_fr }
+                ]
+                  .reduce((reuduced, itm) => {
+                    if (itm.included === 'Yes') {
+                      reuduced.push(itm.txt);
+                    }
+                    return reuduced;
+                  }, [])
+                  .join(' | ')}
+              </p>
+              <p>{item.distributor}</p>
+              <p>{item.phone}</p>
+              <section className={styles.btns}>
+                {item.email && (
+                  <Button
+                    tag='a'
+                    withBorder
+                    target='_blank'
+                    href={`mailto:${item.email}`}
+                  >
+                    <AtSign />
+                  </Button>
+                )}
+                {item.link && (
+                  <Button tag='a' withBorder target='_blank' href={item.link}>
+                    <Globe />
+                  </Button>
+                )}
+                {item.googleLocation && (
+                  <Button
+                    tag='a'
+                    withBorder
+                    target='_blank'
+                    href={item.googleLocation}
+                  >
+                    <MapPin />
+                  </Button>
+                )}
+              </section>
+            </div>
+          </div>
         ))}
-      </Carousel >
-    </FadeIn >
+      </Carousel>
+    </FadeIn>
   );
 };
 
 Distributors.propTypes = {
   color: PropTypes.string.isRequired,
-  data: PropTypes.array,
+  data: PropTypes.array
 };
 
 Distributors.defaultProps = {
-  data: [],
+  data: []
 };
 
-const mapStateToProps = state => ({
-  color: services.vgs.selectors.color(state),
+const mapStateToProps = (state) => ({
+  color: services.vgs.selectors.color(state)
 });
 
-const mapDispatchToProps = dispatch => ({}); // eslint-disable-line
+const mapDispatchToProps = (dispatch) => ({}); // eslint-disable-line
 
-export default compose(
-  connect(mapStateToProps, mapDispatchToProps),
-)(Distributors);
+export default compose(connect(mapStateToProps, mapDispatchToProps))(
+  Distributors
+);
